3a1af127e8bb6fc34fcded3300954d060e99a441
ImNahue <nahuelcarloni25@gmail.com>
2023-11-07 16:37:35 -0300

    merge

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 console.log('hola');
+var persona ={
+    nombre,
+    apellido,
+    sexo,
+    email,
+    celular,
+    comentario
+}
+
 function guardar(){
     let nombre = document.getElementById("nombreIngresado").value;
     let apellido = document.getElementById("apellidoIngresado").value;
@@ -6,12 +15,12 @@ function guardar(){
     let email = document.getElementById("emailIngresado").value;
     let celular = document.getElementById("celularIngresado").value;
     let comentario = document.getElementById("comentarioIngresado").value;
-    document.getElementById("nombre").innerHTML = nombre ;
-    document.getElementById("apellido").innerHTML = apellido ;
-    document.getElementById("sexo").innerHTML = sexo ;
-    document.getElementById("email").innerHTML = email ;
-    document.getElementById("celular").innerHTML = celular ;
-    document.getElementById("comentario").innerHTML = comentario ;
+    // document.getElementById("nombre").innerHTML = nombre ;
+    // document.getElementById("apellido").innerHTML = apellido ;
+    // document.getElementById("sexo").innerHTML = sexo ;
+    // document.getElementById("email").innerHTML = email ;
+    // document.getElementById("celular").innerHTML = celular ;
+    // document.getElementById("comentario").innerHTML = comentario ;
 
 
     let persona ={
@@ -23,4 +32,13 @@ function guardar(){
         comentario
     }
     console.log(persona);
+aumentarContador();
 }
+var contador = 0;
+
+function aumentarContador() {
+    contador++;
+    document.getElementById("counterJS").textContent = contador;
+    counterJS.textContent = contador;
+}
+
